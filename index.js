7384a6b3743181a2c676a4484313213240226d31
Richard Garza <rgactr@gmail.com>
2021-04-09 16:02:50 -0700

    Completed more examples

--- index.js
+++ index.js
@@ -15,3 +15,30 @@ let anything;
 
 // Null
 let nothing = null;
+
+// Object
+
+let richard = {
+  name: 'Richard',
+  age: 25,
+  color: 'Brown',
+  hasSenseOfHumor: true,
+};
+
+// Array
+
+let colors = ['red', 'blue', 'yellow'];
+colors[3] = 'black';
+
+// Functions
+
+const greet = (firstName, lastName) => {
+  console.log(`Hello ${firstName} ${lastName}`);
+};
+
+const add = (firstNum, secondNum) => {
+  return firstNum * secondNum;
+};
+
+greet('John', 'Garza');
+console.log(add(4, 5));
