02e06049f198cf27bc4bcaea3f307ae2f43a1c8b
Richard Garza <rgactr@gmail.com>
2021-04-09 14:56:48 -0700

    Reviewing variable types

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log('Hello World');
+// String
+let name = 'Richard';
+
+let firstName = 'Richard';
+let lastName = 'Garza';
+
+// Number
+const interestRate = 3;
+
+// Boolean
+let isApproved = true;
+
+// Undefined
+let anything;
+
+// Null
+let nothing = null;
